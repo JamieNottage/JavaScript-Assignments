7a614c6297a11e71432f1ece59700429364ec75f
Jamie Nottage <jamie.nottage@outlook.com>
2021-11-23 16:02:18 +0000

    Assignment 7 Complete

---
+++ Assignment-7-bold-hover.js
@@ -0,0 +1,6 @@
+document.getElementById("link").addEventListener("mouseover", () => {
+    document.getElementById("link").style.fontWeight = "bold";
+});
+document.getElementById("link").addEventListener("mouseout", () => {
+    document.getElementById("link").style.fontWeight = "normal";
+});
